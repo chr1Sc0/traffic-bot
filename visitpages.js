c3a794707ed31f6c2621d7044fe5bd9404708d1b
Christian Scotti <cscotti@akamai.com>
2023-10-11 15:23:52 +0200

    Split data from config files

--- visitpages.js
+++ visitpages.js
@@ -1,17 +1,18 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 const data = require('./data.json')
+const config = require('./config.json')
 
 // sleep function
 const sleep = async msec => new Promise(resolve => setTimeout(resolve, msec))
 
-if (data.logging) console.log('Puppeteer starting');
+if (config.logging) console.log('Puppeteer starting');
 
 // Load Stealth Plugin from data file
-if (data.stealth) puppeteer.use(StealthPlugin());
+if (config.stealth) puppeteer.use(StealthPlugin());
 
 //launch puppeteer, do everything in .then() handler
-puppeteer.launch(data.debug ? data.browserOptions.debug : data.browserOptions.headless)
+puppeteer.launch(config.debug ? config.browserOptions.debug : config.browserOptions.headless)
     .then(async function (browser) {
 
   //create a load_page function that returns a promise which resolves when page is ready
@@ -19,7 +20,7 @@ puppeteer.launch(data.debug ? data.browserOptions.debug : data.browserOptions.he
     return new Promise(async function (resolve, reject) {
       // Stealth plugin needs to be opened in a new page 
       const page = await browser.newPage();
-      if (data.logging) console.log(`Opening url ${url}`);
+      if (config.logging) console.log(`Opening url ${url}`);
    
       //wait for the promise to resolve after networkidle2 fires
       await Promise.all([
@@ -27,11 +28,11 @@ puppeteer.launch(data.debug ? data.browserOptions.debug : data.browserOptions.he
           "waitUntil": ["load", "networkidle2"]
         }),
       ]);
-      if (data.logging) console.log('Page loaded');
+      if (config.logging) console.log('Page loaded');
       //extra sleep to allow mPulse beacons to fire
-      await sleep(data.waitTimems);
+      await sleep(config.waitTimems);
       await page.close();
-      if (data.logging) console.log('Page closed');
+      if (config.logging) console.log('Page closed');
       resolve();
     })
   }
@@ -43,7 +44,7 @@ puppeteer.launch(data.debug ? data.browserOptions.debug : data.browserOptions.he
         await load_page(data.urls[i]);
       }
     await browser.close();
-    if (data.logging) console.log('Browser closed');
+    if (config.logging) console.log('Browser closed');
   }
 
   await stepThru();
